d1e6d17d5e5a25722473d9db8c40aa63c06fa4c0
Rizwan khan <khanrizwan5@gmail.com>
2022-07-19 14:44:56 +0530

    2nd

---
+++ src/test/java/myhelloworld/HelloFun.java
@@ -0,0 +1,10 @@
+package myhelloworld;
+
+public class HelloFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hellooooooooooooooooooooooo Wolrdddddddddddddddddddddddddddddddd");
+	}
+
+}
