4d575de37330c26915dd1fff46a097e110ce4c3c
Rizwan khan <khanrizwan5@gmail.com>
2022-07-19 15:17:52 +0530

    3rd commit

---
+++ src/main/java/myhelloworld/MyTime.java
@@ -0,0 +1,11 @@
+package myhelloworld;
+
+public class MyTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("He is not Good");
+
+	}
+
+}
